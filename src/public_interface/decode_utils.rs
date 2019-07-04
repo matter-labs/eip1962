0b27ee6a90b95b1751671151a0ae7b060d316b14
Alex Gluchowski <alex@Alexs-MacBook-Pro.local>
2019-07-04 13:03:37 +0300

    length check enforced

--- src/public_interface/decode_utils.rs
+++ src/public_interface/decode_utils.rs
@@ -5,6 +5,16 @@ use super::constants::*;
 
 use crate::errors::ApiError;
 
+pub(crate) fn split<'a>(bytes: &'a [u8], at: usize, err: &'static str) 
+    -> Result<(&'a [u8], &'a [u8]), ApiError> 
+{
+    if bytes.len() < at {
+        Err(ApiError::InputError(err.to_owned()))
+    } else {
+        Ok(bytes.split_at(at))
+    }
+}
+
 pub(crate) fn decode_biguint_with_length<
     'a
     >
@@ -12,15 +22,9 @@ pub(crate) fn decode_biguint_with_length<
         bytes: &'a [u8], 
     ) -> Result<(BigUint, &'a [u8]), ApiError>
 {
-    if bytes.len() < BYTES_FOR_LENGTH_ENCODING {
-        return Err(ApiError::InputError("Input is not long enough to get modulus length".to_owned()));
-    }
-    let (length_encoding, rest) = bytes.split_at(BYTES_FOR_LENGTH_ENCODING);
+    let (length_encoding, rest) = split(bytes, BYTES_FOR_LENGTH_ENCODING, "Input is not long enough to get modulus length")?;
     let length = length_encoding[0] as usize;
-    if rest.len() < length {
-        return Err(ApiError::InputError("Input is not long enough to get modulus".to_owned()));
-    }
-    let (be_encoding, rest) = rest.split_at(length);
+    let (be_encoding, rest) = split(rest, length, "Input is not long enough to get modulus")?;
     let x = BigUint::from_bytes_be(&be_encoding);
 
     Ok((x, rest))
@@ -33,15 +37,9 @@ pub(crate) fn parse_modulus_and_length<
         bytes: &'a [u8], 
     ) -> Result<(usize, BigUint, &'a [u8]), ApiError>
 {
-    if bytes.len() < BYTES_FOR_LENGTH_ENCODING {
-        return Err(ApiError::InputError("Input is not long enough to get modulus length".to_owned()));
-    }
-    let (length_encoding, rest) = bytes.split_at(BYTES_FOR_LENGTH_ENCODING);
+    let (length_encoding, rest) = split(bytes, BYTES_FOR_LENGTH_ENCODING, "Input is not long enough to get modulus length")?;
     let length = length_encoding[0] as usize;
-    if rest.len() < length {
-        return Err(ApiError::InputError("Input is not long enough to get modulus".to_owned()));
-    }
-    let (be_encoding, rest) = rest.split_at(length);
+    let (be_encoding, rest) = split(rest, length, "Input is not long enough to get modulus")?;
     let x = BigUint::from_bytes_be(&be_encoding);
 
     Ok((length, x, rest))
@@ -63,38 +61,19 @@ pub(crate) fn parse_encodings<'a>(bytes: &'a [u8]) -> Result<(
         BigUint,
         usize,
         &'a [u8]), ApiError> {
-    if bytes.len() < BYTES_FOR_LENGTH_ENCODING {
-        return Err(ApiError::InputError("Input is not long enough to get modulus length".to_owned()));
-    }
-    let (modulus_len, rest) = bytes.split_at(BYTES_FOR_LENGTH_ENCODING);
+    let (modulus_len, rest) = split(bytes, BYTES_FOR_LENGTH_ENCODING, "Input is not long enough to get modulus length")?;
     let modulus_len = modulus_len[0] as usize;
-    if rest.len() < modulus_len {
-        return Err(ApiError::InputError("Input is not long enough to get modulus".to_owned()));
-    }
-    let (modulus_encoding, rest) = rest.split_at(modulus_len);
+    let (modulus_encoding, rest) = split(rest, modulus_len, "Input is not long enough to get modulus")?;
     let modulus = BigUint::from_bytes_be(&modulus_encoding);
     if modulus.is_zero() {
         return Err(ApiError::UnexpectedZero("Modulus can not be zero".to_owned()));
     }
-    if rest.len() < modulus_len {
-        return Err(ApiError::InputError("Input is not long enough to get A parameter".to_owned()));
-    }
-    let (a_encoding, rest) = rest.split_at(modulus_len);
+    let (a_encoding, rest) = split(rest, modulus_len, "Input is not long enough to get A parameter")?;
+    let (b_encoding, rest) = split(rest, modulus_len, "Input is not long enough to get B parameter")?;
 
-    if rest.len() < modulus_len {
-        return Err(ApiError::InputError("Input is not long enough to get B parameter".to_owned()));
-    }
-    let (b_encoding, rest) = rest.split_at(modulus_len);
-
-    if rest.len() < BYTES_FOR_LENGTH_ENCODING {
-        return Err(ApiError::InputError("Input is not long enough to get order length".to_owned()));
-    }
-    let (order_len, rest) = rest.split_at(BYTES_FOR_LENGTH_ENCODING);
+    let (order_len, rest) = split(rest, BYTES_FOR_LENGTH_ENCODING, "Input is not long enough to get order length")?;
     let order_len = order_len[0] as usize;
-    if rest.len() < order_len {
-        return Err(ApiError::InputError("Input is not long enough to get main group order".to_owned()));
-    }
-    let (order_encoding, rest) = rest.split_at(order_len);
+    let (order_encoding, rest) = split(rest, order_len, "Input is not long enough to get main group order")?;
     let order = BigUint::from_bytes_be(&order_encoding);
     if order.is_zero() {
         return Err(ApiError::UnexpectedZero("Main group order can not be zero".to_owned()));
@@ -128,32 +107,20 @@ pub(crate) fn parse_modulus_and_extension_degree<'a>(bytes: &'a [u8]) -> Result<
         u8,
         &'a [u8],
         &'a [u8]), ApiError> {
-    if bytes.len() < BYTES_FOR_LENGTH_ENCODING {
-        return Err(ApiError::InputError("Input is not long enough to get modulus length".to_owned()));
-    }
-    let (modulus_len, rest) = bytes.split_at(BYTES_FOR_LENGTH_ENCODING);
+    let (modulus_len, rest) = split(bytes, BYTES_FOR_LENGTH_ENCODING, "Input is not long enough to get modulus length")?;
     let modulus_len = modulus_len[0] as usize;
-    if rest.len() < modulus_len {
-        return Err(ApiError::InputError("Input is not long enough to get modulus".to_owned()));
-    }
-    let (modulus_encoding, rest) = rest.split_at(modulus_len);
+    let (modulus_encoding, rest) = split(rest, modulus_len, "Input is not long enough to get modulus")?;
     let modulus = BigUint::from_bytes_be(&modulus_encoding);
     if modulus.is_zero() {
         return Err(ApiError::InputError("Modulus can not be zero".to_owned()));
     }
-    if rest.len() < EXTENSION_DEGREE_ENCODING_LENGTH {
-        return Err(ApiError::InputError("Input is not long enough to get extension degree".to_owned()));
-    }
-    let (extension_degree, rest) = rest.split_at(EXTENSION_DEGREE_ENCODING_LENGTH);
+    let (extension_degree, rest) = split(rest, EXTENSION_DEGREE_ENCODING_LENGTH, "Input is not long enough to get extension degree")?;
     let extension_degree = extension_degree[0];
     if !(extension_degree == EXTENSION_DEGREE_2 || extension_degree == EXTENSION_DEGREE_3) {
         return Err(ApiError::InputError("Extension degree must be 2 or 3".to_owned()));
     }
 
-    if rest.len() < modulus_len {
-        return Err(ApiError::InputError("Input is not long enough to Fp non-residue".to_owned()));
-    }
-    let (nonresidue_encoding, rest) = rest.split_at(modulus_len);
+    let (nonresidue_encoding, rest) = split(rest, modulus_len, "Input is not long enough to Fp non-residue")?;
     if rest.len() == 0 {
         return Err(ApiError::InputError("Input is not long enough".to_owned()));
     }
@@ -189,53 +156,28 @@ pub(crate) fn parse_encodings_in_extension<'a>(bytes: &'a [u8]) -> Result<(
         BigUint,
         usize,
         &'a [u8]), ApiError> {
-    if bytes.len() < BYTES_FOR_LENGTH_ENCODING {
-        return Err(ApiError::InputError("Input is not long enough to get modulus length".to_owned()));
-    }
-    let (modulus_len, rest) = bytes.split_at(BYTES_FOR_LENGTH_ENCODING);
+    let (modulus_len, rest) = split(bytes, BYTES_FOR_LENGTH_ENCODING, "Input is not long enough to get modulus length")?;
     let modulus_len = modulus_len[0] as usize;
-    if rest.len() < modulus_len {
-        return Err(ApiError::InputError("Input is not long enough to get modulus".to_owned()));
-    }
-    let (modulus_encoding, rest) = rest.split_at(modulus_len);
+    let (modulus_encoding, rest) = split(rest, modulus_len, "Input is not long enough to get modulus")?;
     let modulus = BigUint::from_bytes_be(&modulus_encoding);
     if modulus.is_zero() {
         return Err(ApiError::InputError("Modulus can not be zero".to_owned()));
     }
-    if rest.len() < EXTENSION_DEGREE_ENCODING_LENGTH {
-        return Err(ApiError::InputError("Input is not long enough to get extension degree".to_owned()));
-    }
-    let (extension_degree, rest) = rest.split_at(EXTENSION_DEGREE_ENCODING_LENGTH);
+    let (extension_degree, rest) = split(rest, EXTENSION_DEGREE_ENCODING_LENGTH, "Input is not long enough to get extension degree")?;
     let extension_degree = extension_degree[0];
     if !(extension_degree == EXTENSION_DEGREE_2 || extension_degree == EXTENSION_DEGREE_3) {
         return Err(ApiError::InputError("Extension degree must be 2 or 3".to_owned()));
     }
 
-    if rest.len() < modulus_len {
-        return Err(ApiError::InputError("Input is not long enough to Fp non-residue".to_owned()));
-    }
-    let (nonresidue_encoding, rest) = rest.split_at(modulus_len);
+    let (nonresidue_encoding, rest) = split(rest, modulus_len, "Input is not long enough to Fp non-residue")?;
 
     let extension_element_len = (extension_degree as usize) * modulus_len;
-    if rest.len() < extension_element_len {
-        return Err(ApiError::InputError("Input is not long enough to get A in extension".to_owned()));
-    }
-    let (a_encoding, rest) = rest.split_at(extension_element_len);
+    let (a_encoding, rest) = split(rest, extension_element_len, "Input is not long enough to get A in extension")?;
+    let (b_encoding, rest) = split(rest, extension_element_len, "Input is not long enough to get B in extension")?;
 
-    if rest.len() < extension_element_len {
-        return Err(ApiError::InputError("Input is not long enough to get B in extension".to_owned()));
-    }
-    let (b_encoding, rest) = rest.split_at(extension_element_len);
-
-    if rest.len() < BYTES_FOR_LENGTH_ENCODING {
-        return Err(ApiError::InputError("Input is not long enough to get group order length".to_owned()));
-    }
-    let (order_len, rest) = rest.split_at(BYTES_FOR_LENGTH_ENCODING);
+    let (order_len, rest) = split(rest, BYTES_FOR_LENGTH_ENCODING, "Input is not long enough to get group order length")?;
     let order_len = order_len[0] as usize;
-    if rest.len() < order_len {
-        return Err(ApiError::InputError("Input is not long enough to size of the main group".to_owned()));
-    }
-    let (order_encoding, rest) = rest.split_at(order_len);
+    let (order_encoding, rest) = split(rest, order_len, "Input is not long enough to size of the main group")?;
     let order = BigUint::from_bytes_be(&order_encoding);
     if order.is_zero() {
         return Err(ApiError::InputError(format!("Main group size can not be zero, file {}, line {}", file!(), line!())));
@@ -262,20 +204,11 @@ pub(crate) fn parse_encodings_in_extension<'a>(bytes: &'a [u8]) -> Result<(
 /// return:
 /// - modulus
 pub(crate) fn parse_curve_type_and_modulus<'a>(bytes: &'a [u8]) -> Result<(u8, BigUint), ApiError> {
-    if bytes.len() < CURVE_TYPE_LENGTH {
-        return Err(ApiError::InputError("Input is not long enough to get curve type".to_owned()));
-    }
-    let (curve_type, rest) = bytes.split_at(CURVE_TYPE_LENGTH);
+    let (curve_type, rest) = split(bytes, CURVE_TYPE_LENGTH, "Input is not long enough to get curve type")?;
     let curve_type = curve_type[0];
-    if bytes.len() < BYTES_FOR_LENGTH_ENCODING {
-        return Err(ApiError::InputError("Input is not long enough to get modulus length".to_owned()));
-    }
-    let (modulus_len, rest) = rest.split_at(BYTES_FOR_LENGTH_ENCODING);
+    let (modulus_len, rest) = split(rest, BYTES_FOR_LENGTH_ENCODING, "Input is not long enough to get modulus length")?;
     let modulus_len = modulus_len[0] as usize;
-    if rest.len() < modulus_len {
-        return Err(ApiError::InputError("Input is not long enough to get modulus".to_owned()));
-    }
-    let (modulus_encoding, rest) = rest.split_at(modulus_len);
+    let (modulus_encoding, rest) = split(rest, modulus_len, "Input is not long enough to get modulus")?;
     let modulus = BigUint::from_bytes_be(&modulus_encoding);
     if modulus.is_zero() {
         return Err(ApiError::InputError("Modulus can not be zero".to_owned()));

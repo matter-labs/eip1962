6dbc40a160b03c1c6c051acdaea2c6c2bd0a72ab
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-02-14 23:10:51 +0300

    new gas metering data

--- run_mnt_miller_loop_metering.sh
+++ run_mnt_miller_loop_metering.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cargo clean -p eth_pairings
-RAYON_NUM_THREADS=4 NUM_SAMPLES=5000 cargo test --release --features=gas_metering_mode -- --nocapture --ignored parallel_measure_miller_loop_pairing_costs_mnt
+RAYON_NUM_THREADS=4 NUM_SAMPLES=3000 cargo test --release --features=gas_metering_mode -- --nocapture --ignored parallel_measure_miller_loop_pairing_costs_mnt
 
